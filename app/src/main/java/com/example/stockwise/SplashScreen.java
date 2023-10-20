af1973e9b417f14c55daaeff5c65a5f28676379d
fdurmaz1 <fikretengindurmaz@gmail.com>
2023-10-19 17:07:26 -0700

    Login & Register pages implemented and committed

--- app/src/main/java/com/example/stockwise/SplashScreen.java
+++ app/src/main/java/com/example/stockwise/SplashScreen.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.stockwise.user.Login;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
@@ -18,7 +20,7 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                startActivity(new Intent(SplashScreen.this, Login.class));
                 finish();
             }
         }, 3000);

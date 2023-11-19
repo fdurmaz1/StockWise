a74fa9734ec6eac3221d98de0e527313fea7c9a1
fdurmaz1 <fikretengindurmaz@gmail.com>
2023-11-18 22:59:47 -0800

    SettingsFragment fetches data from database and showing them on profile management screen

--- app/src/main/java/com/example/stockwise/SettingsFragment.java
+++ app/src/main/java/com/example/stockwise/SettingsFragment.java
@@ -1,64 +1,114 @@
 package com.example.stockwise;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.stockwise.user.Login;
+
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView txtFullName, txtUserName, txtEmail, txtPassword;
+    private int userId; // User ID should be retrieved from SharedPreferences
+    private Button btnSignOut;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // Initialize TextViews
+        txtFullName = view.findViewById(R.id.txtFullName);
+        txtUserName = view.findViewById(R.id.txtUserName);
+        txtEmail = view.findViewById(R.id.txtEmail);
+        txtPassword = view.findViewById(R.id.txtPassword);
+        btnSignOut = view.findViewById(R.id.btnSignOut);
+
+        // Retrieve user ID from SharedPreferences (example shown)
+        userId = getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE).getInt("userid", -1);
+        Log.d("UserInfo", "User ID: " + userId); // Log the user ID
+
+        if (userId != -1) {
+            new FetchUserInfoTask().execute("http://192.168.1.78/LoginRegister/fetch_user_info.php?userid=" + userId);
         }
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOutUser();
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    private void signOutUser() {
+        // Clear shared preferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+
+        redirectToLogin();
+    }
+
+    private void redirectToLogin() {
+        // Replace with your actual navigation logic
+        Intent intent = new Intent(getActivity(), Login.class);
+        startActivity(intent);
+        getActivity().finish();
+    }
+
+    private class FetchUserInfoTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                Log.d("UserInfo", "Response: " + response.toString()); // Log the response
+                return response.toString();
+            } catch (Exception e) {
+                Log.e("UserInfo", "Error: ", e); // Log the error
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                txtFullName.setText(jsonObject.optString("fullname", "N/A"));
+                txtUserName.setText(jsonObject.optString("username", "N/A"));
+                txtEmail.setText(jsonObject.optString("email", "N/A"));
+                // Password is typically not retrieved for display
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Handle error
+            }
+        }
     }
 }

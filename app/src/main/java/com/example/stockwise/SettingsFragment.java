dcb2071a59ce5452b971a2c1130cd1e06f404719
fdurmaz1 <fikretengindurmaz@gmail.com>
2023-11-20 19:07:27 -0800

    Code now prevents users to create new lines when press 'enter' on evry edittext bars ,android:maxLines="1" , android:inputType="text"

--- app/src/main/java/com/example/stockwise/SettingsFragment.java
+++ app/src/main/java/com/example/stockwise/SettingsFragment.java
@@ -17,6 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
 import com.example.stockwise.user.Login;
@@ -33,102 +35,50 @@ import java.net.URLEncoder;
 
 public class SettingsFragment extends Fragment {
 
-    private TextView txtFullName, txtUserName, txtEmail, txtPassword;
-    private ImageView imgFullName, imgUserName, imgEmail, imgPassword;
 
-    private int userId; // User ID should be retrieved from SharedPreferences
-    private Button btnSignOut;
+    private EditText txtFullName, txtUserName, txtEmail, txtPassword;
+    private Button btnUpdate, btnSignOut;
+    private int userId;
+    private String originalPassword; // To store the original password
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        // Initialize TextViews
+        // Initialize Views
         txtFullName = view.findViewById(R.id.txtFullName);
         txtUserName = view.findViewById(R.id.txtUserName);
         txtEmail = view.findViewById(R.id.txtEmail);
         txtPassword = view.findViewById(R.id.txtPassword);
-        btnSignOut = view.findViewById(R.id.btnSignOut);
-        imgFullName = view.findViewById(R.id.imgFullName);
-        imgUserName = view.findViewById(R.id.imgUserName);
-        imgEmail = view.findViewById(R.id.imgEmail);
-        imgPassword = view.findViewById(R.id.imgPassword);
+        btnUpdate = view.findViewById(R.id.btnUpdate);
         btnSignOut = view.findViewById(R.id.btnSignOut);
 
-        // Retrieve user ID from SharedPreferences (example shown)
+
+        // Retrieve user ID from SharedPreferences
         userId = getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE).getInt("userid", -1);
-        Log.d("SettingsFragment", "onCreateView: User ID retrieved: " + userId);
 
         if (userId != -1) {
-            Log.d("SettingsFragment", "onCreateView: Fetching user info");
             new FetchUserInfoTask().execute("http://192.168.1.78/LoginRegister/fetch_user_info.php?userid=" + userId);
         }
 
-        // Set listeners for ImageViews
-        setUpEditListeners();
-
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOutUser();
-            }
-        });
+        btnUpdate.setOnClickListener(v -> updateUserInformation());
+        btnSignOut.setOnClickListener(v -> signOutUser());
 
         return view;
     }
 
-    private void setUpEditListeners() {
-        imgFullName.setOnClickListener(v -> showEditDialog("fullname", txtFullName.getText().toString()));
-        imgUserName.setOnClickListener(v -> showEditDialog("username", txtUserName.getText().toString()));
-        imgEmail.setOnClickListener(v -> showEditDialog("email", txtEmail.getText().toString()));
-        imgPassword.setOnClickListener(v -> showEditDialog("password", txtPassword.getText().toString()));
+
+    private void updateUserInformation() {
+        String fullName = txtFullName.getText().toString();
+        String userName = txtUserName.getText().toString();
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        // Check if password is changed
+        boolean isPasswordChanged = !password.equals(originalPassword);
+
+        new UpdateUserInfoTask().execute(userId, fullName, userName, email, isPasswordChanged ? password : null);
     }
-
-    private void showEditDialog(String field, String currentValue) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Update " + field);
-
-        final EditText input = new EditText(getActivity());
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        input.setText(currentValue);
-        builder.setView(input);
-
-        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                updateUserInfo(field, input.getText().toString());
-                new FetchUserInfoTask().execute("http://192.168.1.78/LoginRegister/fetch_user_info.php?userid=" + userId);
-
-            }
-        });
-        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-
-        builder.show();
-    }
-
-    private void updateUserInfo(String field, String newValue) {
-        // Get current values of all fields
-        String currentFullName = txtFullName.getText().toString();
-        String currentUserName = txtUserName.getText().toString();
-        String currentEmail = txtEmail.getText().toString();
-        String currentPassword = txtPassword.getText().toString(); // Adjust as per your logic
-
-        // Update the value of the field that is being edited
-        if ("fullname".equals(field)) {
-            currentFullName = newValue;
-        } else if ("username".equals(field)) {
-            currentUserName = newValue;
-        } else if ("email".equals(field)) {
-            currentEmail = newValue;
-        } else if ("password".equals(field)) {
-            currentPassword = newValue; // Make sure this is handled securely
-        }
-
-        // Invoke UpdateUserInfoTask with all current values
-        UpdateUserInfoTask(String.valueOf(userId), currentFullName, currentUserName, currentEmail, currentPassword);
-
-    }
-
 
     private void signOutUser() {
         // Clear shared preferences
@@ -162,90 +112,94 @@ public class SettingsFragment extends Fragment {
                     response.append(line);
                 }
                 reader.close();
-                Log.d("UserInfo", "Response: " + response.toString()); // Log the response
                 return response.toString();
             } catch (Exception e) {
-                Log.e("UserInfo", "Error: ", e); // Log the error
+                Log.e("UserInfo", "Error: ", e);
                 return null;
             }
         }
 
         @Override
         protected void onPostExecute(String result) {
-            Log.d("SettingsFragment", "FetchUserInfoTask: Response received");
             if (result == null) {
                 Log.e("SettingsFragment", "FetchUserInfoTask: Result is null");
                 return;
             }
             try {
                 JSONObject jsonObject = new JSONObject(result);
-                txtFullName.setText(jsonObject.optString("fullname", "N/A"));
-                txtUserName.setText(jsonObject.optString("username", "N/A"));
-                txtEmail.setText(jsonObject.optString("email", "N/A"));
-                Log.d("SettingsFragment", "FetchUserInfoTask: User info updated in UI");
+                // Update the EditText fields with the fetched information
+                txtFullName.setText(jsonObject.optString("fullname", ""));
+                txtUserName.setText(jsonObject.optString("username", ""));
+                txtEmail.setText(jsonObject.optString("email", ""));
 
-                // Password is typically not retrieved for display
+                // Assuming the password is not retrieved for security reasons
+                // If you do get the password (hashed or tokenized), you can set it here
+                // originalPassword = jsonObject.optString("passwordToken", "");
+
+                Log.d("SettingsFragment", "FetchUserInfoTask: User info updated in UI");
             } catch (Exception e) {
-                e.printStackTrace();
                 Log.e("SettingsFragment", "FetchUserInfoTask: JSON parsing error", e);
-                // Handle error
             }
         }
     }
 
-    private void UpdateUserInfoTask(String userid, String fullname, String username, String email, String password) {
-        Log.d("SettingsFragment", "UpdateUserInfoTask: Starting AsyncTask");
 
-        // Async task to send data to server
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... voids) {
-                try {
-                    URL url = new URL("http://192.168.1.78/LoginRegister/update_user_info.php");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setDoOutput(true);
-                    OutputStream os = conn.getOutputStream();
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+    private class UpdateUserInfoTask extends AsyncTask<Object, Void, String> {
+        @Override
+        protected String doInBackground(Object... params) {
+            try {
+                URL url = new URL("http://192.168.1.78/LoginRegister/update_user_info.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setDoOutput(true);
 
-                    // POST data
-                    String data = URLEncoder.encode("userid", "UTF-8") + "=" + URLEncoder.encode(userid, "UTF-8") +
-                            "&" + URLEncoder.encode("fullname", "UTF-8") + "=" + URLEncoder.encode(fullname, "UTF-8") +
-                            "&" + URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") +
-                            "&" + URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8") +
-                            "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 
-                    writer.write(data);
-                    writer.flush();
-                    writer.close();
-                    os.close();
+                // Building the data query
+                String data = URLEncoder.encode("userid", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(params[0]), "UTF-8") +
+                        "&" + URLEncoder.encode("fullname", "UTF-8") + "=" + URLEncoder.encode((String) params[1], "UTF-8") +
+                        "&" + URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode((String) params[2], "UTF-8") +
+                        "&" + URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode((String) params[3], "UTF-8");
 
-                    // Get response
-                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        response.append(line);
-                    }
-                    br.close();
-                    return response.toString();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
+                // Include the password in the data only if it's been changed
+                String newPassword = (String) params[4];
+                if (newPassword != null && !newPassword.isEmpty()) {
+                    data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(newPassword, "UTF-8");
                 }
-            }
 
-            @Override
-            protected void onPostExecute(String result) {
-                super.onPostExecute(result);
-                if (result == null) {
-                    Log.e("SettingsFragment", "UpdateUserInfoTask: Result is null");
-                    return;
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    response.append(line);
                 }
-                Log.d("SettingsFragment", "UpdateUserInfoTask: Response received: " + result);
+                br.close();
+                return response.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
             }
-        }.execute();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result != null && result.equals("Update successful")) {
+                Log.d("SettingsFragment", "User info updated successfully");
+                Toast.makeText(getActivity(), "Information updated successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.e("SettingsFragment", "Failed to update user info: " + result);
+                Toast.makeText(getActivity(), "Failed to update information", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
 
 
 }

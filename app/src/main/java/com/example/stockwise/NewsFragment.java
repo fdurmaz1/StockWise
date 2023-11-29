378e6602db9d04c6fdd22097136f187fdf68c8ea
Grizanter <ichrisortilla@hotmail.com>
2023-11-28 17:11:39 -0800

    - committing finished sentiment analysis model and NewsFragment

--- app/src/main/java/com/example/stockwise/NewsFragment.java
+++ app/src/main/java/com/example/stockwise/NewsFragment.java
@@ -1,14 +1,17 @@
 package com.example.stockwise;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
@@ -31,9 +34,9 @@ public class NewsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    Button btnAnalyze;
+    Button btnAnalyze, btnClear;
     EditText editTextInput;
-
+    TextView txtSentimentMessage;
     public NewsFragment() {
         // Required empty public constructor
     }
@@ -63,8 +66,6 @@ public class NewsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
@@ -72,25 +73,77 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View newsView = inflater.inflate(R.layout.fragment_news, container, false);
-        if(!Python.isStarted()) {
+        if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(newsView.getContext()));
         }
 
-        final Python py =Python.getInstance();
+        final Python py = Python.getInstance();
 
         btnAnalyze = newsView.findViewById(R.id.btnAnalyze);
         editTextInput = newsView.findViewById(R.id.editTextInput);
+        txtSentimentMessage = newsView.findViewById(R.id.txtSentimentMessage);
+
         btnAnalyze.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println(editTextInput.getText().toString());
                 String txt = editTextInput.getText().toString();
                 PyObject pyo = py.getModule("sentiment_analysis");
-                PyObject obj = pyo.callAttr("analyze_sentiment", txt);
-                System.out.println(obj.toString());
-                Toast.makeText(getContext(), txt, Toast.LENGTH_LONG).show();
+                PyObject obj = pyo.callAttr("analyze_article", txt);
+                int sentimentScore = obj.toInt();
+                System.out.println(sentimentScore);
+                String sentimentText = processSentimentScore(sentimentScore);
+                txtSentimentMessage.setText(sentimentText);
+                if (sentimentScore > 2) {
+                    txtSentimentMessage.setTextColor(Color.parseColor("#008000"));
+                } else if (sentimentScore < 2) {
+                    txtSentimentMessage.setTextColor(Color.parseColor("#FF0000"));
+                } else {
+                    txtSentimentMessage.setTextColor(Color.parseColor("#000000"));
+                }
             }
         });
+
+        btnClear = newsView.findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtSentimentMessage.setText("");
+                editTextInput.setText("");
+            }
+        });
+
         return newsView;
+    }
+
+    public String processSentimentScore(int sentimentScore) {
+        String sentimentText = "";
+        String veryBadEmoji = new String(Character.toChars(0x1F61E));
+        String badEmoji = new String(Character.toChars(0x1F62A));
+        String neutralEmoji = new String(Character.toChars(0x1F610));
+        String goodEmoji = new String(Character.toChars(0x1F642));
+        String veryGoodEmoji = new String(Character.toChars(0x1F600));
+        String failedEmoji = new String(Character.toChars(0x1F937));
+
+        switch(sentimentScore) {
+            case 0:
+                sentimentText = veryBadEmoji + " Very Bad News " + veryBadEmoji;
+                break;
+            case 1:
+                sentimentText = badEmoji + " Bad News " + badEmoji;
+                break;
+            case 2:
+                sentimentText = neutralEmoji + " Neutral News " + neutralEmoji;
+                break;
+            case 3:
+                sentimentText = goodEmoji + " Good News " + goodEmoji;
+                break;
+            case 4:
+                sentimentText = veryGoodEmoji + " Very Good News " + veryGoodEmoji;
+                break;
+            default:
+                sentimentText = failedEmoji + " Sentiment Analysis Failed " + failedEmoji;
+        }
+
+        return sentimentText;
     }
 }
